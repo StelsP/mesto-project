2030549efc0f9c1e7e12441bdfedf4001a699d39
slava-k <stelspeh@gmail.com>
2021-08-21 20:22:15 +0300

    better profile-edit-form

--- popup.js
+++ popup.js
@@ -13,18 +13,38 @@ let inputName = document.querySelector('.popup__input_type_name');
 let inputQuote = document.querySelector('.popup__input_type_quote');
 
 
+
+
 editButton.addEventListener('click', () => {
   popup.classList.add('popup_opened');
 });
 
-closeButtonPopup.addEventListener('click', () => {
+if (profileName.textContent === ('Жак-Ив Кусто')) {
+
+  closeButtonPopup.addEventListener('click', () => {
+    popup.classList.remove('popup_opened');
+    inputName.value = ('');
+    inputQuote.value = ('');
+    inputName.placeholder = profileName.textContent;
+    inputQuote.placeholder = profileQuote.textContent;
+  });
+
+} else {
   popup.classList.remove('popup_opened');
-});
+}
 
 submitButtonPopup.addEventListener('click', () => {
   popup.classList.remove('popup_opened');
   profileName.textContent = (inputName.value);
   profileQuote.textContent = (inputQuote.value);
+  inputName.value = ('');
+  inputQuote.value = ('');
+  inputName.placeholder = profileName.textContent;
+  inputQuote.placeholder = profileQuote.textContent;
 });
+
+
+
+
 
 
