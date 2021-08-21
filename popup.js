57d57dfa2e1370bee2ae7ebe7bb6446d4a8005d8
slava-k <stelspeh@gmail.com>
2021-08-21 20:37:49 +0300

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,17 +1,15 @@
-let editButton = document.querySelector('.profile__edit-button');
-let closeButtonPopup = document.querySelector('.popup__close-button');
-let submitButtonPopup = document.querySelector('.popup__submit-button');
-
-let popup = document.querySelector('.popup');
-
-let profileName = document.querySelector('.profile__name');
-let profileQuote = document.querySelector('.profile__quote');
-
-let inputName = document.querySelector('.popup__input_type_name');
-let inputQuote = document.querySelector('.popup__input_type_quote');
-
-
 function buttons () {
+  let editButton = document.querySelector('.profile__edit-button');
+  let closeButtonPopup = document.querySelector('.popup__close-button');
+  let submitButtonPopup = document.querySelector('.popup__submit-button');
+
+  let popup = document.querySelector('.popup');
+
+  let profileName = document.querySelector('.profile__name');
+  let profileQuote = document.querySelector('.profile__quote');
+
+  let inputName = document.querySelector('.popup__input_type_name');
+  let inputQuote = document.querySelector('.popup__input_type_quote');
 
   editButton.addEventListener('click', () => {
     popup.classList.add('popup_opened');
@@ -33,8 +31,7 @@ function buttons () {
     inputQuote.value = ('');
     inputName.placeholder = profileName.textContent;
     inputQuote.placeholder = profileQuote.textContent;
-});
-
+  });
 }
 
 buttons ();
