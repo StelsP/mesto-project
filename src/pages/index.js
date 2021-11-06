465478e3aeb0bea04c980554ff72f6c10495655e
stelsp <stelspeh@gmail.com>
2021-11-07 02:11:50 +0300

    install babel

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import '../components/modal.js';
-import '../components/utils.js';
-import '../components/cards.js';
-import { enableValidation } from '../components/validate.js';
+// import modal from './components/modal.js';
+// import utils from './components/utils.js';
+// import cards from './components/cards.js';
+// import { enableValidation } from './components/validate.js';
 
-enableValidation();
+// enableValidation();
 
-
+console.log('Hello, World!')
 
 
 
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path'); // подключаем path к конфигу вебпак
+
+// module.exports — это синтаксис экспорта в Node.js
+module.exports = {
+  entry: {
+    main: './src/pages/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+        publicPath: ''
+// указали в какой файл будет собираться весь js и дали ему имя
+  },
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+    compress: true, // это ускорит загрузку в режиме разработки
+    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+
+    open: true // сайт будет открываться сам при запуске npm run dev
+  }, // добавили режим разработчика
+
+}
