a73ee7c106ca6ecdbc4093761be144ed0483ce9e
stelsp <stelspeh@gmail.com>
2021-11-06 23:07:27 +0300

    fix close popup on esc/overlayClick/closeBtnClick

--- scripts/utils.js
+++ scripts/utils.js
@@ -2,13 +2,13 @@
 
 export function openPopup(popup) {
   popup.classList.add('popup_opened');
-  document.addEventListener('click', closePopupWidthOverlayClick);
+  document.addEventListener('mousedown', closePopupWidthOverlayClick);
   document.addEventListener('keydown', closePopupWidthEscape);
 }
 
 export function closePopup(popup) {
   popup.classList.remove('popup_opened');
-  document.removeEventListener('click', closePopupWidthOverlayClick);
+  document.removeEventListener('mousedown', closePopupWidthOverlayClick);
   document.removeEventListener('keydown', closePopupWidthEscape);
 }
 
@@ -21,11 +21,8 @@ const closePopupWidthEscape = (e) => {
 
 const closePopupWidthOverlayClick = (e) => {
 
-  const isCloseButtonClicked = e.target.classList.contains('.popup__close-button');
-  const isOverlayClicked = e.target.classList.contains('.popup');
-
-  if (isCloseButtonClicked || isOverlayClicked) {
-    submitPopup(popup);
+  if (e.target.classList.contains('popup')) {
+    closePopup(e.target);
   }
 }
 
