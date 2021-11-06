91c03d99a288830efa2d39c9d5ec031974eaf52f
stelsp <stelspeh@gmail.com>
2021-11-07 00:22:17 +0300

    create src folder

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import '../components/modal.js';
+import '../components/utils.js';
+import '../components/cards.js';
+import { enableValidation } from '../components/validate.js';
+
+enableValidation();
+
+
+
+
+
+
