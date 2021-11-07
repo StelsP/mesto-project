86daa658bed99d5c799dbe5a3d63a9d91fc5671e
stelsp <stelspeh@gmail.com>
2021-11-07 03:08:53 +0300

    set up js/html/fonts/images/css via webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,11 @@
-// import modal from './components/modal.js';
-// import utils from './components/utils.js';
-// import cards from './components/cards.js';
-// import { enableValidation } from './components/validate.js';
+import '../components/modal';
+import '../components/utils.js';
+import '../components/cards.js';
+import { enableValidation } from '../components/validate.js';
+import './index.css';
 
-// enableValidation();
-
-console.log('Hello, World!')
+enableValidation();
 
 
-const numbers = [2, 3, 5];
 
-// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
-const doubledNumbers = numbers.map(number => number * 2);
-
-console.log(doubledNumbers); // 4, 6, 10
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
-const path = require('path'); // подключаем path к конфигу вебпак
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-// module.exports — это синтаксис экспорта в Node.js
 module.exports = {
   entry: {
     main: './src/pages/index.js'
@@ -9,27 +11,39 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
         publicPath: ''
-// указали в какой файл будет собираться весь js и дали ему имя
   },
   mode: 'development',
   devServer: {
-    static: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
-    compress: true, // это ускорит загрузку в режиме разработки
-    port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+    static: path.resolve(__dirname, './dist'),
+    compress: true,
+    port: 8080,
 
-    open: true // сайт будет открываться сам при запуске npm run dev
-  }, // добавили режим разработчика
+    open: true
+  },
   module: {
-    rules: [ // rules — это массив правил
-      // добавим в него объект правил для бабеля
+    rules: [
       {
-        // регулярное выражение, которое ищет все js файлы
         test: /\.js$/,
-        // при обработке этих файлов нужно использовать babel-loader
         use: 'babel-loader',
-        // исключает папку node_modules, файлы в ней обрабатывать не нужно
         exclude: '/node_modules/'
-      }
-      ]
-  }
-}
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader'
+        }]
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ]
+};
