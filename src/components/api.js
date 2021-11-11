d2cbcf50792d39e62cc73367e1c109e030dd0782
stelsp <stelspeh@gmail.com>
2021-11-11 21:37:01 +0300

    fetch/add profile name,quote

--- src/components/api.js
+++ src/components/api.js
@@ -1,19 +1,21 @@
-// // GET USER DATA
-// fetch('https://mesto.nomoreparties.co/v1/plus-cohort-3/users/me', {
-//   headers: {
-//     authorization: 'bb5f0ee9-ef64-4836-93fe-6fc2439d86be',
-//     'Content-Type': 'application/json; charset=UTF-8'
-//     }
-// })
-//   .then(res => res.json())
-//   .then((res) => {
-//      console.log(res);
-//   });
+// GET USER DATA
+import { profileName } from './var.js'
+import { profileQuote } from './var.js'
+fetch('https://mesto.nomoreparties.co/v1/plus-cohort-3/users/me', {
+  headers: {
+    authorization: 'bb5f0ee9-ef64-4836-93fe-6fc2439d86be',
+    'Content-Type': 'application/json; charset=UTF-8'
+    }
+})
+  .then(res => res.json())
+  .then((res) => {
+    profileName.textContent = res.name;
+    profileQuote.textContent = res.about;
+  });
 
 // GET CARDS DATA
 import { elementsList } from "./var";
 import { createCard } from "./cards";
-
 export function createInitialCards() {
   fetch('https://mesto.nomoreparties.co/v1/plus-cohort-3/cards', {
   headers: {
@@ -28,7 +30,6 @@ export function createInitialCards() {
     });
   });
 }
-
 
 
 
