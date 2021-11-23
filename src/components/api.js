817e16464620619911eb2abf515861a2bb4873e9
Vilo4ka <saiviolet@gmail.com>
2021-11-23 22:33:27 +0300

    test

--- src/components/api.js
+++ src/components/api.js
@@ -1,89 +1,92 @@
-const config = {
-  baseUrl: 'https://mesto.nomoreparties.co/v1/plus-cohort-3',
-  headers: {
-    authorization: 'bb5f0ee9-ef64-4836-93fe-6fc2439d86be',
-    'Content-Type': 'application/json'
+import { config } from './var.js'
+
+export default class Api {
+  constructor(config) {
+    this.config = config;
   }
-}
 
-const checkRes = (res) => {
-  if (res.ok) {
-    return res.json();
+  // CHECK RESULT
+  checkRes(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status + " - " + res.statusText}`);
   }
-  return Promise.reject(`Ошибка: ${res.status + " - " + res.statusText}`);
+
+  // GET USER DATA
+  getProfileInfo() {
+    return fetch(`${this.config.baseUrl}/users/me`, {
+      headers: this.config.headers,
+    })
+    .then(checkRes)
+  }
+
+  // GET CARDS DATA
+  getInitialCards() {
+    return fetch(`${this.config.baseUrl}/cards`, {
+      headers: this.config.headers,
+    })
+    .then(checkRes)
+  }
+
+  // PATCH USER DATA
+  patchProfileInfo(name, quote) {
+    return fetch(`${this.config.baseUrl}/users/me`, {
+      method: 'PATCH',
+      headers: this.config.headers,
+      body: JSON.stringify({
+        name: name.value,
+        about: quote.value
+      }),
+    })
+    .then(checkRes)
+  }
+
+  // PATCH USER AVATAR
+  patchProfilePhoto(image) {
+    return fetch(`${this.config.baseUrl}/users/me/avatar`, {
+      method: 'PATCH',
+      headers: this.config.headers,
+      body: JSON.stringify({
+        avatar: image.value,
+      }),
+    })
+    .then(checkRes)
+  }
+
+  // POST NEW CARD
+  exporpostNewCards(name, link) {
+    return fetch(`${config.baseUrl}/cards`, {
+      method: 'POST',
+      headers: config.headers,
+      body: JSON.stringify({
+        name: name.value,
+        link: link.value,
+      }),
+    })
+    .then(checkRes)
+  }
+
+  // DELETE CARD
+  deleteCardHandler(cardId) {
+    return fetch(`${config.baseUrl}/cards/${cardId}`, {
+      method: 'DELETE',
+      headers: config.headers,
+    })
+    .then(checkRes)
+  }
+
+  // PUT LIKE
+  likeHandler(cardId, method) {
+    return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
+      method: method,
+      headers: config.headers,
+    })
+    .then(checkRes)
+  }
+
 }
 
-// GET USER DATA
-export const getProfileInfo = () => {
-  return fetch(`${config.baseUrl}/users/me`, {
-    headers: config.headers,
-  })
-  .then(checkRes)
-}
-
-// GET CARDS DATA
-export const getInitialCards = () => {
-  return fetch(`${config.baseUrl}/cards`, {
-    headers: config.headers,
-  })
-  .then(checkRes)
-}
-
-// PATCH USER DATA
-export const patchProfileInfo = (name, quote) => {
-  return fetch(`${config.baseUrl}/users/me`, {
-    method: 'PATCH',
-    headers: config.headers,
-    body: JSON.stringify({
-      name: name.value,
-      about: quote.value
-    }),
-  })
-  .then(checkRes)
-}
-
-// PATCH USER AVATAR
-export const patchProfilePhoto = (image) => {
-  return fetch(`${config.baseUrl}/users/me/avatar`, {
-    method: 'PATCH',
-    headers: config.headers,
-    body: JSON.stringify({
-      avatar: image.value,
-    }),
-  })
-  .then(checkRes)
-}
-
-// POST NEW CARD
-export const postNewCards = (name, link) => {
-  return fetch(`${config.baseUrl}/cards`, {
-    method: 'POST',
-    headers: config.headers,
-    body: JSON.stringify({
-      name: name.value,
-      link: link.value,
-    }),
-  })
-  .then(checkRes)
-}
-
-// DELETE CARD
-export const deleteCardHandler = (cardId) => {
-  return fetch(`${config.baseUrl}/cards/${cardId}`, {
-    method: 'DELETE',
-    headers: config.headers,
-  })
-  .then(checkRes)
-}
-
-// PUT LIKE
-export const likeHandler = (cardId, method) => {
-  return fetch(`${config.baseUrl}/cards/likes/${cardId}`, {
-    method: method,
-    headers: config.headers,
-  })
-  .then(checkRes)
-}
 
 
 
