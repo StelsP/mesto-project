d524784e23be2bb71185b08adc4d74d2f4fdf6de
stelsp <stelspeh@gmail.com>
2021-12-04 15:24:46 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,19 +26,22 @@ import {
   imageInput,
   titleInput,
   elementsForm,
+  // CLASS API
   api,
+  // CLASS POPUP
   profilePhotoEditPopup,
   profileEditPopup,
   elementsAddPopup,
   imagePopup,
+  // CLASS CARD
   elementsTemplate,
+  // CLASS FORM VALIDATOR
   configElementsValidation,
   } from '../components/var.js';
 import './index.css';
 import { Card } from '../components/cards.js';
 import { Section } from '../components/section.js';
 import { FormValidator } from '../components/validate.js';
-// import { UserInfo } from '../components/UserInfo.js';
 
 // GET PROFILE/CARDS INFO
 Promise.all([api.getProfileInfo(), api.getInitialCards()])
@@ -64,24 +67,49 @@ addInitialCards.renderingItems();
   console.log(err);
 });
 
-// CLOSE FULLSCREEN CARD IMAGE
+// CLOSE
+// PROFILE PHOTO EDIT FORM
+profilePhotoCloseButton.addEventListener('click', () => {
+  profilePhotoEditPopup.close(profilePhotoEditForm);
+});
+// PROFILE EDIT FORM
+profileCloseButton.addEventListener('click', () => {
+  profileEditPopup.close(profileEditForm);
+});
+// ADD NEW CARDS FORM
+elementsCloseButton.addEventListener('click', () => {
+  elementsAddPopup.close(elementsAddForm);
+});
+// FULLSCREEN CARD IMAGE
 imageCloseButton.addEventListener('click', () => {
   imagePopup.close(image);
 });
 
+// OPEN
 // PROFILE PHOTO EDIT FORM
 profilePhotoEditButton.addEventListener('click', () => {
   const getValidProfilePhotoEditForm = new FormValidator(configElementsValidation, profilePhotoEditForm);
   getValidProfilePhotoEditForm.enableValidation();
   profilePhotoEditPopup.open(profilePhotoEditForm);
 });
-
-profilePhotoCloseButton.addEventListener('click', () => {
-  profilePhotoEditPopup.close(profilePhotoEditForm);
+// PROFILE EDIT FORM
+profileEditButton.addEventListener('click', () => {
+  const getValidProfileEditForm = new FormValidator(configElementsValidation, profileEditForm);
+  getValidProfileEditForm.enableValidation();
+  nameInput.value = profileName.textContent;
+  quoteInput.value = profileQuote.textContent;
+  profileEditPopup.open(profileEditForm);
+});
+// ADD NEW CARDS FORM
+elementsAddButton.addEventListener('click', () => {
+  const getValidelementsAddForm = new FormValidator(configElementsValidation, elementsAddForm);
+  getValidelementsAddForm.enableValidation();
+  elementsAddPopup.open(elementsAddForm);
 });
 
+// SUBMIT
+// PROFILE PHOTO EDIT FORM
 profilePhotoEditForm.addEventListener('submit', (e) => {
-
   e.preventDefault();
   renderLoading(true, profilePhotoEditForm);
     api.patchProfilePhoto(photoInput)
@@ -97,20 +125,7 @@ profilePhotoEditForm.addEventListener('submit', (e) => {
         renderLoading(false, profilePhotoEditForm, 'Сохранить', true);
       });
 });
-
 // PROFILE EDIT FORM
-profileEditButton.addEventListener('click', () => {
-  const getValidProfileEditForm = new FormValidator(configElementsValidation, profileEditForm);
-  getValidProfileEditForm.enableValidation();
-  nameInput.value = profileName.textContent;
-  quoteInput.value = profileQuote.textContent;
-  profileEditPopup.open(profileEditForm);
-});
-
-profileCloseButton.addEventListener('click', () => {
-  profileEditPopup.close(profileEditForm);
-});
-
 profileEditForm.addEventListener('submit', (e) => {
   e.preventDefault();
   renderLoading(true, profileEditForm);
@@ -127,18 +142,7 @@ profileEditForm.addEventListener('submit', (e) => {
         renderLoading(false, profileEditForm, 'Сохранить', true);
       });
 });
-
 // ADD NEW CARDS FORM
-elementsAddButton.addEventListener('click', () => {
-  const getValidelementsAddForm = new FormValidator(configElementsValidation, elementsAddForm);
-  getValidelementsAddForm.enableValidation();
-  elementsAddPopup.open(elementsAddForm);
-});
-
-elementsCloseButton.addEventListener('click', () => {
-  elementsAddPopup.close(elementsAddForm);
-});
-
 elementsAddForm.addEventListener('submit', (e) => {
   e.preventDefault();
   renderLoading(true, elementsAddForm);
@@ -157,6 +161,7 @@ elementsAddForm.addEventListener('submit', (e) => {
       });
 });
 
+// RENDER LOADING
 function renderLoading(isLoading, form, text, disabled) {
   const submitButton = form.querySelector('.popup__submit-button')
   submitButton.disabled = disabled;
