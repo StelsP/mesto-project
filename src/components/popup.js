48a3e06052e00a91a4ca0411d274aad76a3a8994
stelsp <stelspeh@gmail.com>
2021-12-06 23:51:24 +0300

    Update popup.js

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,31 +1,18 @@
-// OPEN/CLOSE POPUP
-export class Popup {
-  constructor(popup) {
-    this.popup = popup;
+import {Popup} from './popup.js';
+
+export class PopupWithImage extends Popup{
+  constructor(selector) {
+    super(selector);
+
+    this.popupImage = this.popup.querySelector('.image__pic');
+    this.popupTitle = this.popup.querySelector('.image__title');
   }
 
-  open() {
-    this.popup.classList.add('popup_opened');
-    document.addEventListener('mousedown', this._closeWidthOverlayClick);
-    document.addEventListener('keydown', this._closeWidthEscape);
-  };
-
-  close() {
-    this.popup.classList.remove('popup_opened');
-    document.removeEventListener('mousedown', this._closeWidthOverlayClick);
-    document.removeEventListener('keydown', this._closeWidthEscape);
-  };
-
-  _closeWidthEscape = (e) => {
-    if (e.key === "Escape") {
-      this.close();
-    };
-  };
-
-  _closeWidthOverlayClick = (e) => {
-    if (e.target.classList.contains('popup')) {
-      this.close();
-    };
-  };
+  openPopup({link, name}) {
+    super.openPopup();
+    this.popupImage.src = link;
+    this.popupImage.alt = name;
+    this.popupTitle.textContent = name;
+  }
 }
 
